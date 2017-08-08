8700123fefce7f1095bd98960db2c094321da211
s.kuebel <s.kuebel@otterbach.de>
2017-08-08 15:57:14 +0200

    Nächster Versuch eine Sortierklasse funktionierend zu basteln.

---
+++ src/SchnellSort.java
@@ -0,0 +1,11 @@
+
+public class SchnellSort {
+
+	private static char[] buchstabenkette;
+	private static int lange;
+
+	public static char[] sortieren(char[] zeichenkette) {
+
+		return ausgabe;
+	}
+}
